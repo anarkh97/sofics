bd8e253f4bc012e825328b8cf4f99830d71d589d
Aditya Narkhede <124534030+anarkh97@users.noreply.github.com>
2024-11-22 15:11:36 -0500

    Full optimization test case included. Working on test case readme.

---
+++ tests/short_async_evaluations/myconfig.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Add comment here about available directory variables
+
+TEMPLATE_DIR=$LAUNCH_DIR/templates
+
+EVALUATION_CONCURRENCY=$(
+  grep "evaluation_concurrency" $LAUNCH_DIR/dakota.in |
+  cut -d "=" -f 2 
+)
+
+# setup size and executables for fluid solver
+M2C_SIZE=56
+M2C_INPUT=input.st
+SHOCK_INPUT=SphericalShock.txt
+
+# setup size and executables for solid solver
+AEROS_SIZE=8
+AEROS_INPUT=fem.in
+
+# setup executable and input file for FE mesher
+GMSH_EXE=gmsh
+GMSH_INPUT=struct.geo
